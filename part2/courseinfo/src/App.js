7925f10b40a3d8417b0747933ea887467026879f
saiupendra2210 <84955333+saiupendra2210@users.noreply.github.com>
2021-06-08 12:01:20 +0530

    Update App.js

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
           name: 'Fundamentals of React',
           exercises: 10,
           id: 1
-        },
+        };
         {
           name: 'Using props to pass data',
           exercises: 7,
@@ -26,8 +26,8 @@ const App = () => {
           name: 'Redux',
           exercises: 11,
           id: 4
-        }
-      ]
+        };
+      ];
     },
     {
       name: 'Node.js',
@@ -37,22 +37,25 @@ const App = () => {
           name: 'Routing',
           exercises: 3,
           id: 1
-        },
+        };,
         {
           name: 'Middlewares',
           exercises: 7,
           id: 2
-        }
-      ]
-    }
-  ]
+        };
+
+
+      ];
+    };
+  ];
 
   return (
     <>
       <h1>Web development curriculum</h1>
       {courses.map(course => <Course course={course} />)}
+
     </>
   )
-}
+};
 
 export default App
